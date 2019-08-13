5762efd2b8265ded4821dcca149df9302b21165e
rahul <rahul@nodexperts.com>
2019-08-13 12:21:50 +0530

    added the express, graphql

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 const app = express();
-app.listen(4000, () => { 
+
+app.use('/graphql', graphqlHTTP({}));
+app.listen(4000, () => {
     console.log('-Listening on port 4000-')
-})
+});
